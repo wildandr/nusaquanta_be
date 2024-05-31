c03ad56590f35d6934d04da0bfc1e15030a7a12b
Darriel Markerizal <darrielmarkerizal@gmail.com>
2024-06-01 01:15:39 +0700

    initiate project

---
+++ src/api/project-detail/routes/project-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-detail router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::project-detail.project-detail');
---
+++ src/api/project-detail/services/project-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-detail service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::project-detail.project-detail');
