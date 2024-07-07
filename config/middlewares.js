cc333b6e98528e4202bd365e0f349772f3cf2fbc
Rasyid Kusnady <54275103+rasyidk@users.noreply.github.com>
2024-07-07 15:52:34 +0700

    fix middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,23 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["https://nusaquanta.com", "http://localhost:3000"], // Tambahkan URL frontend kamu di sini
+      headers: [
+        "Content-Type",
+        "Authorization",
+        "Origin",
+        "Accept",
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Methods",
+        "Access-Control-Allow-Headers",
+      ],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
