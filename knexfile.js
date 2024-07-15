b4e0d1265dfccbf40fac111034d4c2c52a370710
Ubuntu <ubuntu@ip-172-31-8-145.ap-southeast-2.compute.internal>
2024-07-15 10:08:42 +0000

    update db

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./dev.sqlite3",
+      filename: "./db.sqlite",
     },
     useNullAsDefault: true,
   },
